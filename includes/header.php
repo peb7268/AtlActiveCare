b1adc68dce80912708f6a36b2566b54f0d0ad571
Paul Barrick <peb7268@gmail.com>
2013-11-26 01:16:14 -0500

    New patient page 80% done.

--- includes/header.php
+++ includes/header.php
@@ -9,3 +9,41 @@
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,200,800,900' rel='stylesheet' type='text/css'>
 </head>
 <body>
+
+
+<h2 id="header-meta">Active Release Technique Provider > Johns Creek Chiropractor > Sports Injuries and Auto Accident > 6290 Abbotts Bridge Rd. #204 Johns Creek, GA 30097 > <span class="large">t: 770-559-4236</span></h1>
+
+    <ul id="header" class="sections clearfix">
+        <li>
+            <div id="logo">
+                ACTIVE
+                CARE
+                <span class="tagline">ATLANTA</span>
+                
+                <span class="sub-message">
+                    CHIROPRACTIC
+                    <span class="sub-message">
+                        JOINT &amp; SOFT TISSUE
+                    </span>
+                </span>
+            </div>
+        </li>
+        <li>
+            <ul id="nav" class="clearfix">
+                <li><a href="#">OUR DOCTORS</a></li>
+                <li><a href="#">SERVICES</a></li>
+                <li><a href="#">ARTICLES</a></li>
+                <li><a href="#">GALLERY</a></li>
+                <li><a href="new_patient.php">NEW PATIENT</a></li>
+                <li><a href="#">TESTIMONIALS</a></li>
+                <li><a href="#">NEWS/EVENTS</a></li>
+                <li><a href="#">HOURS/CONTACT</a></li>
+            </ul>
+        </li>
+        <li>
+            <p>Dr. Lee Chiropractic is now </p>
+            <p>ACTIVE CARE ATLANTA</p>
+            <p><em>Same Location and Same Doctors</em></p>
+            <p> >> November Sat. Openings: 9th and 23rd.</p>        
+        </li>
+    </ul>
--- index.php
+++ index.php
@@ -1,46 +1,18 @@
 <?php require "includes/header.php" ?>
-<h2 id="header-meta">Active Release Technique Provider > Johns Creek Chiropractor > Sports Injuries and Auto Accident > 6290 Abbotts Bridge Rd. #204 Johns Creek, GA 30097 > <span class="large">t: 770-559-4236</span></h1>
-
-    <ul id="header" class="sections clearfix">
-        <li>
-            <div id="logo">
-                ACTIVE
-                CARE
-                <span class="tagline">ATLANTA</span>
-                
-                <span class="sub-message">
-                    CHIROPRACTIC
-                    <span class="sub-message">
-                        JOINT &amp; SOFT TISSUE
-                    </span>
-                </span>
-            </div>
-        </li>
-        <li>
-            <ul id="nav" class="clearfix">
-                <li><a href="#">OUR DOCTORS</a></li>
-                <li><a href="#">SERVICES</a></li>
-                <li><a href="#">ARTICLES</a></li>
-                <li><a href="#">GALLERY</a></li>
-                <li><a href="#">NEW PATIENT</a></li>
-                <li><a href="#">TESTIMONIALS</a></li>
-                <li><a href="#">NEWS/EVENTS</a></li>
-                <li><a href="#">HOURS/CONTACT</a></li>
-            </ul>
-        </li>
-        <li>
-            <p>Dr. Lee Chiropractic is now </p>
-            <p>ACTIVE CARE ATLANTA</p>
-            <p><em>Same Location and Same Doctors</em></p>
-            <p> >> November Sat. Openings: 9th and 23rd.</p>        
-        </li>
-    </ul>
     <div id="hero">
         <?php randomHero(); ?>
         <h4>
             Active Care Atlanta specializes in Sports Injuries, Performance Care and Auto Accident Injuries. Our doctors are among the few chiropractors in Georgia that are certified in Active Release Techniques (ART®). ART® is a state-of-the-art soft tissue release system based treatment designed for overused muscles, tendons, ligaments, fascia, and nerves.
         </h4>
     </div>
+    <ul id="footer_sprite" class="clearfix">
+        <li id="active_release"><a href="#"></a></li>
+        <li id="car_accident"><a href="#"></a></li>
+        <li id="crossfit_care"><a href="#"></a></li>
+        <li id="dance_medicine"><a href="#"></a></li>
+        <li id="pregnancy"><a href="#"></a></li>
+        <li id="massage"><a href="#"></a></li>
+    </ul>
 <?php require "includes/footer.php" ?>
 
 
