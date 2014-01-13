2d558cd7f31c5b30c51037314b717a63ae99cf50
Paul Barrick <peb7268@gmail.com>
2014-01-13 17:22:37 -0500

    changed some style rules and added some social stuff

---
+++ wp-content/themes/AtlActiveCare/lib/social.php
@@ -0,0 +1,30 @@
+<div id="socialWidgetWrapper">
+    <h4>Social</h4>
+    <div id="social_buttons" class="clearfix">
+        <ul id="links" class="left clearfix">
+            <li>
+                <a href="https://www.facebook.com/ActiveCareAtlanta" target="_blank">Facebook</a>
+            </li>
+            <li>
+                <a href="https://twitter.com/activecareatl" target="_blank">Twitter</a>
+            </li>
+            <li>
+                <a href=" https://plus.google.com/108273537002822560144/posts" target="_blank">Google+</a>
+            </li>
+            <li>
+                <a href="https://maps.google.com/maps?expflags=enable_star_based_justifications:true&ie=UTF8&cid=10918895883272021449&q=Active+Care+Atlanta&iwloc=A&gl=US&hl=en" target="_blank">Find Us</a>
+            </li>
+        </ul>
+        <ul id="buttons" class="left clearfix">
+            <li>
+                <div class="fb-like" data-href="http://www.activecareatl.com" data-width="100" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false" data-colorscheme="dark"></div>
+            </li>
+            <li>
+                <a href="https://twitter.com/activecareatl" class="twitter-follow-button" data-show-count="true" data-lang="en">Follow @activecareatl</a>
+            </li>
+            <li>
+                <div class="g-plusone" data-size="medium" data-annotation="inline" data-width="120"></div>
+            </li>
+        </ul>
+    </div>
+</div>
